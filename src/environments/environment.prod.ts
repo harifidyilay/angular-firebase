72dd1a2c59ade05a70379bbf26ea9f746d00c884
Harifidy Desk <harifidyilay@gmail.com>
2024-12-04 16:15:24 -0500

    Initial commit ...

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAq_5DPjknlSMi83yP_MN21RmGnEe8AeEc',
+    authDomain: 'fir-demo-7acfb.firebaseapp.com',
+    projectId: 'fir-demo-7acfb',
+    storageBucket: 'fir-demo-7acfb.firebasestorage.app',
+    messagingSenderId: '279859546807',
+    appId: '1:279859546807:web:14c52f35c6d528ff391b32',
+    measurementId: 'G-XTVQ6KV5EK',
+  },
+};
